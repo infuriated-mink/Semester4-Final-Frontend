ea4f6d4fc2c6d3c47646e6d62d16226aee35ecbf
Evan <evan.harte@keyin.com>
2024-08-06 11:38:26 -0230

    functioning admin page

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Events from "./components/Events";
 import AdminPage from "./pages/AdminPage";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-    <Route path="/" element={<Events />} />
-    <Route path="/admin" element={<AdminPage />} />
-  </Routes>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Events />} />
+        <Route path="/admin" element={<AdminPage />} />
+      </Routes>
+    </Router>
   );
 }
 
